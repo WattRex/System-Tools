bbda3d1a3ba01654ab9f9602ad0e324925c1a664
Marius Crisan <mariuscrsn@gmail.com>
2023-08-10 12:29:11 +0200

    Temp fix increment version

--- .github/actions/build-package/get_version.py
+++ .github/actions/build-package/get_version.py
@@ -44,8 +44,8 @@ def merge_version():
         new_version = checkout_version
     else:
         new_version = pypi_version
-        patch = int(new_version[-1]) + 1
-        new_version = f'{new_version[:-1]}{patch}'
+    patch = int(new_version[-1]) + 1
+    new_version = f'{new_version[:-1]}{patch}'
 
     print(f"New version package is: {new_version}")
     subprocess.run(f'echo new-version={new_version} >> $GITHUB_OUTPUT', shell=True, check=False)
