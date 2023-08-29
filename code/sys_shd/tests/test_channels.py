32a03916b779c96a37307acca568f071e6f957d5
RobertoAldea <r.aldea.csic@gmail.com>
2023-08-29 09:49:08 +0100

    Minor change configuring log in test

--- code/sys_shd/tests/test_channels.py
+++ code/sys_shd/tests/test_channels.py
@@ -18,7 +18,7 @@ from pytest import fixture, mark
 #######################      SYSTEM ABSTRACTION IMPORTS  #######################
 from system_logger_tool import Logger, SysLogLoggerC, sys_log_logger_get_module_logger
 
-main_logger = SysLogLoggerC(file_log_levels="code/log_config.yaml")
+main_logger = SysLogLoggerC(file_log_levels="code/sys_shd/tests/log_config.yaml")
 log: Logger = sys_log_logger_get_module_logger(name="test_shd")
 
 #######################       THIRD PARTY IMPORTS        #######################
