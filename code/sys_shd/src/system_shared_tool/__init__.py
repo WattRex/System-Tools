f21508a60372f2597e11267c3cc72b9503cced3f
RobertoAldea <r.aldea.csic@gmail.com>
2023-09-29 10:07:52 +0100

    Change state enum to status

--- code/sys_shd/src/system_shared_tool/__init__.py
+++ code/sys_shd/src/system_shared_tool/__init__.py
@@ -2,8 +2,8 @@
 This file specifies what is going to be exported from this module.
 In this case is sys_shd.
 """
-from .sys_shd_common import SysShdErrorC, SysShdNodeParamsC, SysShdNodeC, SysShdNodeStateE
+from .sys_shd_common import SysShdErrorC, SysShdNodeParamsC, SysShdNodeC, SysShdNodeStatusE
 from .sys_shd_channels import SysShdIpcChanC, SysShdChanC
 from .sys_shd_objects import SysShdSharedObjC
-__all__= ['SysShdChanC', 'SysShdSharedObjC', 'SysShdErrorC', 'SysShdNodeStateE',
+__all__= ['SysShdChanC', 'SysShdSharedObjC', 'SysShdErrorC', 'SysShdNodeStatusE',
           'SysShdIpcChanC', 'SysShdNodeParamsC', 'SysShdNodeC']
